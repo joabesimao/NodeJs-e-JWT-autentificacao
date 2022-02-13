b994103e84b3bfdfc0a8228a4adacdff4109bc07
JoabeSimao <simaojoabecosta@gmail.com>
2022-02-13 19:43:37 -0300

    criar app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const { estrategiasAutenticacao } = require("./src/usuarios")
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+module.exports = app;
